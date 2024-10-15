007a77f8acce7e14f69901aaaeb206de6b176592
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2024-10-14 21:51:44 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 
-import { defaultTheme } from "./styles/default";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   
@@ -12,6 +13,8 @@ export function App() {
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
